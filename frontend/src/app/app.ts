deede76afb289bd6c9407313c186c3152697420a
Mateusz <mateuszgogacz51@gmail.com>
2025-12-21 20:31:16 +0100

    Tabela html

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,30 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common'; // <--- 1. To jest ten słownik!
+import { TicketService } from './ticket.service';
+import { Ticket } from './ticket.model';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule], // <--- 2. Tutaj musimy go użyć!
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('frontend');
+export class AppComponent implements OnInit {
+  
+  tickets: Ticket[] = [];
+
+  constructor(private ticketService: TicketService) {}
+
+  ngOnInit() {
+    this.ticketService.getTickets().subscribe({
+      next: (data) => {
+        this.tickets = data;
+        console.log('Sukces! Pobrane dane:', data);
+      },
+      error: (err) => {
+        console.error('Błąd pobierania:', err);
+      }
+    });
+  }
 }
