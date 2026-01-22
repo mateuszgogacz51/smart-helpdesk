20d8656bb506e7d987d1ecb56df7012e93415966
Mateusz <mateuszgogacz51@gmail.com>
2026-01-22 21:07:00 +0100

    Errorr

--- src/main/java/pl/gogacz/smart_helpdesk/config/WebConfig.java
+++ src/main/java/pl/gogacz/smart_helpdesk/config/WebConfig.java
@@ -5,7 +5,5 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    // TUTA BYŁ BŁĄD: Usunęliśmy metodę addCorsMappings.
-    // Teraz całą konfiguracją CORS zarządza poprawnie SecurityConfig.java.
-    // Ten plik musi pozostać pusty (poza definicją klasy).
+    // Pusto! Nie dodawaj tu żadnego kodu!
 }
